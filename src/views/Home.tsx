9d89c4a86b7ac190005373ffa0e4bb9de93ed79c
Ezequiel Brito Navarro <ebritonavarro@gmail.com>
2026-04-22 09:53:24 -0400

    feat: implement home view

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Container, Typography, Box } from '@material-ui/core';
 
-const Home = () => {
+const Home: React.FC = () => {
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Container 
+      maxWidth="md" 
+      style={{ 
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        height: '80vh'
+      }}
+    >
+      <Box textAlign="center">
+        <Typography 
+          variant="h2" 
+          color="primary" 
+          gutterBottom 
+          style={{ fontWeight: 700, letterSpacing: '-1px' }}
+        >
+          Bienvenido
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
